afaf5d6f700a4aff6d1000aaa236c1bdd318bae5
pompocity <msmit181@calpoly.edu>
2019-09-26 17:56:20 -0700

    Add files via upload

---
+++ Part1/SimpleIf.java
@@ -0,0 +1,18 @@
+class SimpleIf
+{
+    public static double max(double x, double y)
+    {
+        /* TO DO: Write an if statement to determine which
+         argument is larger and return that value.
+      */
+        if ( y > x){
+            return y;
+        }
+        if (x > y) {
+            return x;
+        }
+        else {
+            return 0;
+        }
+    }
+}
